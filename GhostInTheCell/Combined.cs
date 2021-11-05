f4dfb82d2e3e076d5f2b1a448eaf6c95071b215e
Terry malone <terry.malone@red-gate.com>
2021-11-05 09:34:08 +0000

    Basic strategy

--- GhostInTheCell/Combined.cs
+++ GhostInTheCell/Combined.cs
@@ -71,11 +71,60 @@ using System.Collections;
         {
             var move = "WAIT";
 
-            DisplayFactories();
+            //DisplayFactories();
 
-            var mostPopulaceFactory = _factories.Where(f => f.Owner == Owner.Player).OrderByDescending(f => f.NumberOfCyborgs).First();
+            // Basic strategy
 
-            var amount = mostPopulaceFactory.NumberOfCyborgs / 2;
+            // We can make one move per turn
+            // Pick in this order
+
+            // Send from most populace factory
+            var sendFromFactory = _factories.Where(f => f.Owner == Owner.Player).OrderByDescending(f => f.NumberOfCyborgs).First();
+
+            var allViableTargetFactories = _factories.Where(f => f.Owner != Owner.Player)
+                                                     .OrderByDescending(f => f.Owner == Owner.Neutral).ToList();             // Neutral then opponent
+
+            // closest then furthest
+            var viableLinks = sendFromFactory.Links.OrderByDescending(l => l.Distance);
+
+            var targetIndex = 0;
+            var targetFound = false;
+
+            var availableTroops = sendFromFactory.NumberOfCyborgs;
+
+            var targetId = 0;
+            var troopsToSend = 0;
+
+            while (!targetFound)
+            {
+                Console.Error.WriteLine($"targetIndex: {targetIndex}");
+                Console.Error.WriteLine($"allViableTargetFactories.Count: {allViableTargetFactories.Count}");
+                var targetFactory = allViableTargetFactories[targetIndex];
+                // TODO: don't send if we're already sending
+                    // unless opponent is sending to beat us
+                var neededTroops = targetFactory.NumberOfCyborgs + 10; //TODO: add for any en-route
+
+                if(neededTroops <= availableTroops)
+                {
+                    targetId = targetFactory.Id;
+                    troopsToSend = neededTroops;
+                    targetFound = true;
+                }
+                else // Temporary just send what we can
+                {
+                    targetId = targetFactory.Id;
+                    troopsToSend = sendFromFactory.NumberOfCyborgs;
+                    targetFound = true;
+                }
+
+                targetIndex++;
+            }
+
+            // How many to send
+            // target amount + enemy troops en-route + 10
+                // If we don't have enough pick another one
+
+            move = $"MOVE {sendFromFactory.Id} {targetId} {troopsToSend}";
 
             return move;
         }
@@ -115,7 +164,6 @@ using System.Collections;
                 Console.Error.WriteLine("================");
             }
         }
-
     }
 
     internal sealed class Link

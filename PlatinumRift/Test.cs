9cbfe2c85ab927e1efbad09813cc23fbb951804b
Terry Malone <terry.malone@red-gate.com>
2022-02-25 12:34:05 +0000

    Set up Platinum Rift project

---
+++ PlatinumRift/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PlatinumRift
+{
+    public class Test
+    {
+        public void Testing()
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            var numbers = new int[N];
+
+            for (var i = 0; i < N; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            string test = "  ^  ";
+
+            test.IndexOf()
+
+            var count = 0;
+            var start = numbers[0];
+            var currentIndex = start;
+
+            while (count <= 300)
+            {
+                var num = numbers[currentIndex];
+
+                currentIndex = num;
+
+                if (currentIndex == start)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+
+            int.TryParse()
+            }
+
+            Console.WriteLine("false");
+        }
+    }
+}

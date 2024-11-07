03bad7beb4fc222f15ea27780932d9d7ed98c5bb
Terry Malone <terrysmalone@hotmail.com>
2024-11-07 13:43:07 +0000

    Upgrade connect 4 tests to .net 8

--- Connect4Tests/ConnectFourTests.cs
+++ Connect4Tests/ConnectFourTests.cs
@@ -1,103 +1,102 @@
+namespace Connect4Tests; 
+
 using Connect4;
 using NUnit.Framework;
 
-namespace Connect4Tests
+public class ConnectFourTests
 {
-    public class ConnectFourTests
+    [Test]
+    public void TestEvaluate_VerticalWinFor0([Range(1,10)]int depth)
     {
-        [Test]
-        public void TestEvaluate_VerticalWinFor0([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.SetMoveSequence("0101010");
+        var connect4 = new ConnectFour();
+        
+        connect4.SetMoveSequence("0101010");
 
-            var result = connect4.Evaluate(is0: true, depth);
-            
-            Assert.That(result, Is.EqualTo(depth + 1));
-        }
+        var result = connect4.Evaluate(is0: true, depth);
         
-        [Test]
-        public void TestEvaluate_VerticalLossFor0([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.SetMoveSequence("0101010");
-            
-            var result = connect4.Evaluate(is0: false, depth);
-            
-            Assert.That(result, Is.EqualTo(-depth - 1));
-        }
+        Assert.That(result, Is.EqualTo(depth + 1));
+    }
+    
+    [Test]
+    public void TestEvaluate_VerticalLossFor0([Range(1,10)]int depth)
+    {
+        var connect4 = new ConnectFour();
         
-        [Test]
-        public void TestEvaluate_VerticalWinFor1([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.SetMoveSequence("05150515");
-            
-            var result = connect4.Evaluate(is0: false, depth);
-            
-            Assert.That(result, Is.EqualTo(depth + 1));
-        }
+        connect4.SetMoveSequence("0101010");
         
-        [Test]
-        public void TestEvaluate_VerticalLossFor1([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.SetMoveSequence("43454041");
+        var result = connect4.Evaluate(is0: false, depth);
+        
+        Assert.That(result, Is.EqualTo(-depth - 1));
+    }
+    
+    [Test]
+    public void TestEvaluate_VerticalWinFor1([Range(1,10)]int depth)
+    {
+        var connect4 = new ConnectFour();
+        
+        connect4.SetMoveSequence("05150515");
+        
+        var result = connect4.Evaluate(is0: false, depth);
+        
+        Assert.That(result, Is.EqualTo(depth + 1));
+    }
+    
+    [Test]
+    public void TestEvaluate_VerticalLossFor1([Range(1,10)]int depth)
+    {
+        var connect4 = new ConnectFour();
+        
+        connect4.SetMoveSequence("43454041");
 
-            var result = connect4.Evaluate(is0: false, depth);
-            
-            Assert.That(result, Is.EqualTo(-depth - 1));
-        }
+        var result = connect4.Evaluate(is0: false, depth);
         
-        [Test]
-        public void TestEvaluate_HorizontalWinFor0([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.AddMove(3, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(4, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(5, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(6, 0);
-            
-            var result = connect4.Evaluate(is0: true, depth);
-            
-            Assert.That(result, Is.EqualTo(depth + 1));
-        }
+        Assert.That(result, Is.EqualTo(-depth - 1));
+    }
+    
+    [Test]
+    public void TestEvaluate_HorizontalWinFor0([Range(1,10)]int depth)
+    {
+        var connect4 = new ConnectFour();
         
-        [Test]
-        public void TestEvaluate_HorizontalLossFor0([Range(1,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            
-            connect4.AddMove(3, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(4, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(5, 0);
-            connect4.AddMove(3, 1);
-            connect4.AddMove(6, 0);
-            
-            var result = connect4.Evaluate(is0: true, depth);
-            
-            Assert.That(result, Is.EqualTo(depth + 1));
-        }
+        connect4.AddMove(3, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(4, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(5, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(6, 0);
+        
+        var result = connect4.Evaluate(is0: true, depth);
+        
+        Assert.That(result, Is.EqualTo(depth + 1));
+    }
+    
+    [Test]
+    public void TestEvaluate_HorizontalLossFor0([Range(1,10)]int depth)
+    {
+        var connect4 = new ConnectFour();
+        
+        connect4.AddMove(3, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(4, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(5, 0);
+        connect4.AddMove(3, 1);
+        connect4.AddMove(6, 0);
+        
+        var result = connect4.Evaluate(is0: true, depth);
+        
+        Assert.That(result, Is.EqualTo(depth + 1));
+    }
 
-        [Test]
-        public void TestEvaluate_BugWin()
-        {
-            var connect4 = new ConnectFour();
-            connect4.SetMoveSequence("11230122627252");
+    [Test]
+    public void TestEvaluate_BugWin()
+    {
+        var connect4 = new ConnectFour();
+        connect4.SetMoveSequence("11230122627252");
 
-            var result = connect4.Evaluate(is0: true);
-            
-            Assert.That(result, Is.EqualTo(-1));
-        }
+        var result = connect4.Evaluate(is0: true);
+        
+        Assert.That(result, Is.EqualTo(-1));
     }
 }
--- Connect4Tests/MoveCalculatorTests.cs
+++ Connect4Tests/MoveCalculatorTests.cs
@@ -1,22 +1,21 @@
-﻿using Connect4;
+﻿namespace Connect4Tests; 
+
+using Connect4;
 using NUnit.Framework;
 
-namespace Connect4Tests
+[TestFixture]
+public class MoveCalculatorTests
 {
-    [TestFixture]
-    public class MoveCalculatorTests
+    [Test]
+    public void TestEvaluate_Bug([Range(2,10)]int depth)
     {
-        [Test]
-        public void TestEvaluate_Bug([Range(2,10)]int depth)
-        {
-            var connect4 = new ConnectFour();
-            connect4.SetMoveSequence("112301226272");
-            // In the game my bot played 5 next, letting the opponent then win with 2
-            
-            var moveCalculator = new MoveCalculator();
-            var results = moveCalculator.GetMoveScoresUsingAlphaBeta(connect4, depth, 0);
-            
-            //Assert.That(result, Is.EqualTo(depth + 1));
-        }
+        var connect4 = new ConnectFour();
+        connect4.SetMoveSequence("112301226272");
+        // In the game my bot played 5 next, letting the opponent then win with 2
+        
+        var moveCalculator = new MoveCalculator();
+        var results = moveCalculator.GetMoveScoresUsingAlphaBeta(connect4, depth, 0);
+        
+        //Assert.That(result, Is.EqualTo(depth + 1));
     }
 }

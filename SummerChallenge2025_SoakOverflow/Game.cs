38d82c616b1f158c55804ade055f0bf052f38f14
Terry Malone <terrysmalone@hotmail.com>
2025-07-17 17:21:05 +0100

    Got to Wood 2

---
+++ SummerChallenge2025_SoakOverflow/Game.cs
@@ -0,0 +1,123 @@
+﻿
+namespace SummerChallenge2025_SoakOverflow;
+
+class Game
+{
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    public int MyId { get; private set; }
+
+    List<Agent> playerAgents = new List<Agent>();
+    List<Agent> opponentAgents = new List<Agent>();
+
+    public Game(int myId)
+    {
+        MyId = myId;
+    }
+
+    public void SetGameSize(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    internal void AddAgent(int id, int player, int shootCooldown, int optimalRange, int soakingPower, int splashBombs)
+    {
+        if (player == MyId)
+        {
+            Console.Error.WriteLine($"Adding player agent {id} with cooldown {shootCooldown}, range {optimalRange}, soaking power {soakingPower}, splash bombs {splashBombs}");
+            playerAgents.Add(new Agent(id, player, shootCooldown, optimalRange, soakingPower, splashBombs));
+        }
+        else
+        {
+            Console.Error.WriteLine($"Adding opponent agent {id} with cooldown {shootCooldown}, range {optimalRange}, soaking power {soakingPower}, splash bombs {splashBombs}");
+            opponentAgents.Add(new Agent(id, player, shootCooldown, optimalRange, soakingPower, splashBombs));
+        }
+    }
+
+    internal void MarkAllAgentsForCulling()
+    {
+        foreach (var agent in playerAgents)
+        {
+            agent.InGame = false;
+        }
+
+        foreach (var agent in opponentAgents)
+        {
+            agent.InGame = false;
+        }
+    }
+
+    internal void DestroyMarkedAgents()
+    {
+        Console.Error.WriteLine("Destroying marked agents...");
+        Console.Error.WriteLine($"Player agents before culling: {playerAgents.Count}");
+        Console.Error.WriteLine($"Opponent agents before culling: {opponentAgents.Count}");
+        playerAgents.RemoveAll(agent => !agent.InGame);
+        opponentAgents.RemoveAll(agent => !agent.InGame);
+        Console.Error.WriteLine($"Player agents after culling: {playerAgents.Count}");
+        Console.Error.WriteLine($"Opponent agents after culling: {opponentAgents.Count}");
+    }
+
+    internal void UpdateAgent(int agentId, int x, int y, int cooldown, int splashBombs, int wetness)
+    {
+        Agent agent = playerAgents.FirstOrDefault(a => a.Id == agentId) 
+            ?? opponentAgents.FirstOrDefault(a => a.Id == agentId);
+
+        if (agent != null)
+        {
+            agent.UpdatePosition(x, y);
+            agent.ShootCooldown = cooldown;
+            agent.SplashBombs = splashBombs;
+            agent.Wetness = wetness;
+            agent.InGame = true; // Mark it as still in game
+        }
+    }
+
+    // One line per agent: <agentId>;<action1;action2;...> actions are "MOVE x y | SHOOT id | THROW x y | HUNKER_DOWN | MESSAGE text"
+    internal List<string> GetMoves()
+    {
+        Console.Error.WriteLine($"Player agent count: {playerAgents.Count}");
+        Console.Error.WriteLine($"Opponent agent count: {opponentAgents.Count}");
+
+        Console.Error.WriteLine("In get moves");
+        List<string> moves = new List<string>();
+
+        int wettestOpponent = GetWettestOpponentId();
+
+        foreach (var agent in playerAgents)
+        {
+            // Example action: Move towards a fixed position
+            if (agent.Id == playerAgents[0].Id)
+            {
+                moves.Add($"{agent.Id};SHOOT {wettestOpponent}");
+            }
+            else
+            {
+                moves.Add($"{agent.Id};SHOOT {wettestOpponent}");
+            }
+
+            // To debug: Console.Error.WriteLine("Debug messages...");
+        }
+
+        return moves;
+    }
+
+    private int GetWettestOpponentId()
+    {
+        int wettestOpponentId = -1;
+        int maxWetness = -1;
+
+        foreach (var agent in opponentAgents)
+        {
+            if (agent.Wetness > maxWetness)
+            {
+                maxWetness = agent.Wetness;
+                wettestOpponentId = agent.Id;
+            }
+        }
+
+        return wettestOpponentId;
+    }
+}
---
+++ SummerChallenge2025_SoakOverflow/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Net.WebSockets;
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SummerChallenge2025_SoakOverflow;
+
+/**
+ * Win the water fight by controlling the most territory, or out-soak your opponent!
+ **/
+class Player
+{
+ 
+
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        int myId = int.Parse(Console.ReadLine()); // Your player id (0 or 1)
+        int agentDataCount = int.Parse(Console.ReadLine()); // Total number of agents in the game
+
+        var game = new Game(myId);
+
+        for (int i = 0; i < agentDataCount; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int agentId = int.Parse(inputs[0]); // Unique identifier for this agent
+            int player = int.Parse(inputs[1]); // Player id of this agent
+            int shootCooldown = int.Parse(inputs[2]); // Number of turns between each of this agent's shots
+            int optimalRange = int.Parse(inputs[3]); // Maximum manhattan distance for greatest damage output
+            int soakingPower = int.Parse(inputs[4]); // Damage output within optimal conditions
+            int splashBombs = int.Parse(inputs[5]); // Number of splash bombs this can throw this game
+
+            game.AddAgent(agentId, player, shootCooldown, optimalRange, soakingPower, splashBombs);
+        }
+
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]); // Width of the game map
+        int height = int.Parse(inputs[1]); // Height of the game map
+
+        game.SetGameSize(width, height);
+
+        for (int i = 0; i < height; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            for (int j = 0; j < width; j++)
+            {
+                int x = int.Parse(inputs[3 * j]);// X coordinate, 0 is left edge
+                int y = int.Parse(inputs[3 * j + 1]);// Y coordinate, 0 is top edge
+                int tileType = int.Parse(inputs[3 * j + 2]);
+            }
+        }
+
+        int count = 1;
+
+        // game loop
+        while (true)
+        {
+            // This is a workaround for a weird bug I found. Hopefully They'll fix it
+            if (count > 1)
+            {
+                Console.ReadLine();
+            }
+            Console.Error.WriteLine("Starting new game loop...");
+            game.MarkAllAgentsForCulling();
+
+            int agentCount = int.Parse(Console.ReadLine()); // Total number of agents still in the game
+            Console.Error.WriteLine($"Processing {agentCount} agents...");
+            for (int i = 0; i < agentCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                Console.Error.WriteLine($"Processing agent data: {string.Join(", ", inputs)}");
+                int agentId = int.Parse(inputs[0]);
+                int x = int.Parse(inputs[1]);
+                int y = int.Parse(inputs[2]);
+                int cooldown = int.Parse(inputs[3]); // Number of turns before this agent can shoot
+                int splashBombs = int.Parse(inputs[4]);
+                int wetness = int.Parse(inputs[5]); // Damage (0-100) this agent has taken
+
+                game.UpdateAgent(agentId, x, y, cooldown, splashBombs, wetness);
+            }
+
+            game.DestroyMarkedAgents();
+
+            List<string> moves = game.GetMoves();
+            foreach (var move in moves)
+            {
+                Console.WriteLine(move);
+            }
+
+            count++;
+        }
+    }
+}

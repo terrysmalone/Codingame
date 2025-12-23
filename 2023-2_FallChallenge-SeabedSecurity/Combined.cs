a4c42e3e0212668498d26e5e496f36a077353f2a
Terry Malone <terrysmalone@hotmail.com>
2025-12-23 21:08:49 +0000

    Sink then rise

--- 2023-2_FallChallenge-SeabedSecurity/Combined.cs
+++ 2023-2_FallChallenge-SeabedSecurity/Combined.cs
@@ -7,8 +7,8 @@ using System.Drawing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
 using System.Text;
+using System.IO;
 using System.Collections;
 using System.Runtime.CompilerServices;
 
@@ -156,6 +156,11 @@ internal class Game
     private DirectionCalculator directionCalculator;
 
     internal int visibleCreatureCount;
+    internal int round = 0;
+
+    internal bool earlyGame = true;
+
+    internal Dictionary<int, int> lastRoundTorchUsed = new();
 
     internal void InitialiseCreatures(List<Creature> allCreatures)
     {
@@ -211,54 +216,97 @@ internal class Game
 
     internal List<string> CalculateActions()
     {
+        Console.Error.WriteLine($"Visible creatures this turn: {visibleCreatureCount}");
+
+        round++;
+
         var actions = new List<string>();
 
-        for (var i = 0; i < myDrones.Count; i++)
+        if (earlyGame)
         {
-            var drone = myDrones[i];
-            Logger.Drone(drone);
-
-            // If stored scans >= 4 head to surface
-            if (drone.ScannedCreaturesIds.Count >= 4 || MyScannedCreatureIds.Count + drone.ScannedCreaturesIds.Count >= 12)
+            // in the early game drop as far as possible, picking up fish thenrise to the top
+            for (var i = 0; i < myDrones.Count; i++)
             {
-                actions.Add($"MOVE {drone.Position.X} 500 0");
+                var drone = myDrones[i];
+
+                var lightLevel = CalculateLightLevel(drone);
+
+                // Drop to 8,000+ then rise to the top
+                if (drone.Position.Y < 8000)
+                {
+                    actions.Add($"MOVE {drone.Position.X} 9000 {lightLevel}");
+                }
+                else
+                {
+                    actions.Add($"MOVE {drone.Position.X} 500 {lightLevel}");
+                    earlyGame = false;
+                }
             }
-            else
+        }
+        else
+        {
+            // in the mid game be more deliberate about where to go
+            for (var i = 0; i < myDrones.Count; i++)
             {
-                var lightLevel = 0;
+                var drone = myDrones[i];
+                var lightLevel = CalculateLightLevel(drone);
 
-                if(visibleCreatureCount > 0)
+                // If stored scans >= 4 head to surface
+                if (drone.ScannedCreaturesIds.Count >= 4 || MyScannedCreatureIds.Count + drone.ScannedCreaturesIds.Count >= 12)
                 {
-                    lightLevel = 1;
+                    actions.Add($"MOVE {drone.Position.X} 500 {lightLevel}");
                 }
-
-
-                // Move in the direction of the most unscanned fish
-                CreatureDirection direction = directionCalculator.GetBestDirectionFromRadarBlips(drone);
-
-                var targetPosition = new Point(0, 0);
-
-                switch (direction)
+                else
                 {
-                    case CreatureDirection.TL:
-                        targetPosition = new Point(drone.Position.X - 1000, drone.Position.Y - 1000);
-                        break;
-                    case CreatureDirection.TR:
-                        targetPosition = new Point(drone.Position.X + 1000, drone.Position.Y - 1000);
-                        break;
-                    case CreatureDirection.BL:
-                        targetPosition = new Point(drone.Position.X - 1000, drone.Position.Y + 1000);
-                        break;
-                    case CreatureDirection.BR:
-                        targetPosition = new Point(drone.Position.X + 1000, drone.Position.Y + 1000);
-                        break;
-                }
+                    // Move in the direction of the most unscanned fish
+                    CreatureDirection direction = directionCalculator.GetBestDirectionFromRadarBlips(drone);
 
-                actions.Add($"MOVE {targetPosition.X} {targetPosition.Y} {lightLevel} {drone.BatteryLevel}");
+                    var targetPosition = new Point(0, 0);
+
+                    switch (direction)
+                    {
+                        case CreatureDirection.TL:
+                            targetPosition = new Point(drone.Position.X - 1000, drone.Position.Y - 1000);
+                            break;
+                        case CreatureDirection.TR:
+                            targetPosition = new Point(drone.Position.X + 1000, drone.Position.Y - 1000);
+                            break;
+                        case CreatureDirection.BL:
+                            targetPosition = new Point(drone.Position.X - 1000, drone.Position.Y + 1000);
+                            break;
+                        case CreatureDirection.BR:
+                            targetPosition = new Point(drone.Position.X + 1000, drone.Position.Y + 1000);
+                            break;
+                    }
+
+                    actions.Add($"MOVE {targetPosition.X} {targetPosition.Y} {lightLevel} {drone.BatteryLevel}");
+                }
             }
         }
 
         return actions;
+    }
+
+    private int CalculateLightLevel(Drone drone)
+    {
+        var lightLevel = 0;
+
+        if (lastRoundTorchUsed.ContainsKey(drone.Id))
+        {
+            var lastUsed = lastRoundTorchUsed[drone.Id];
+
+            if (round - lastUsed >= 5)
+            {
+                lightLevel = 1;
+                lastRoundTorchUsed[drone.Id] = round;
+            }
+        }
+        else
+        {
+            lastRoundTorchUsed[drone.Id] = 0;
+        }
+
+        return lightLevel;
     }
 }
 

9f0294847318857d16810223e3b46522ee9efd36
Terry Malone <terrysmalone@hotmail.com>
2026-03-30 22:31:31 +0100

    Use minimax

--- Connect4/Game.cs
+++ Connect4/Game.cs
@@ -3,41 +3,66 @@
 // https://www.codingame.com/multiplayer/bot-programming/connect-4
 internal sealed class Game
 {
-    private int _depth = 5;
-    
+    private int _depth = 3;
+
+    private int _rows = 7;
+    private int _columns = 9;
+
     private int _myId;
     private int _oppId;
-    
-    private ConnectFour _connectFour;
-    private MoveCalculator _calculator;
+    private MiniMax _miniMax;
+    GameState _gameState;
+
+    private int _turn = 0;
     
     internal Game(int myId)
     {
         _myId = myId;
         _oppId = myId == 0 ? 1 : 0;
-        
-        _connectFour = new ConnectFour();
-        _calculator = new MoveCalculator();
+
+        _miniMax = new MiniMax();
+        _gameState = new GameState(_rows, _columns);
     }
     
-    internal string GetMove()
+    internal string GetMove(int[,] board)
     {
-        int bestMove = _calculator.GetBestMoveUsingAlphaBeta(_connectFour, _depth, _myId);
+        Logger.StartRoundStopwatch();
+        Logger.Board(board);
 
-        _connectFour.AddMove(bestMove, _myId);
-        
-        //_connectFour.PrintBoard();
-        
+        if (_turn <= 1)
+        {
+            if (_myId == 1)
+            {
+                if (_turn == 0)
+                {
+                    return "4";
+                }
+            }
+            else
+            {
+                if (_turn == 1)
+                {
+                    if (board[6, 4] == 1)
+                    {
+                        return "STEAL";
+                    }
+                    else
+                    {
+                        return "4";
+                    }
+                }
+            }
+        }
+
+        _gameState.SetGameState(board, _myId);
+
+        int bestMove = _miniMax.FindBestMove(_gameState, _depth);
+
         return bestMove.ToString();
     }
 
-    internal void AddOpponentMove(int column)
+    internal void SetTurnIndex(int turnIndex)
     {
-        _connectFour.AddMove(column, _oppId);
-    }
-    
-    public void AddOpponentSteal()
-    {
-        _connectFour.Steal(_oppId);
+        _turn = turnIndex;
     }
 }
